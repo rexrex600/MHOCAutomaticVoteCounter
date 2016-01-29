b41c7433b433b9e7374f5a0dda86c6880dccbfcd
WadeMarshall <wmmnola@gmail.com>
2016-01-28 18:09:11 -0600

    Changed it up a bit

--- main.py
+++ main.py
@@ -10,8 +10,46 @@ scope = ['https://spreadsheets.google.com/feeds']
 credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
 r = praw.Reddit("Vote Counting Bot")
 gc = gspread.authorize(credentials)
+sh = gc.open('Copy of MHoC Master Sheet')
+wks = sh.worksheet("7th Govt Voting Record")
+r.login('agentnola','')
+#print("Input thread id")
+#thread = raw_input()
 
-wks = gc.open_by_key('1Z6gg99ViOVNuSDzO77wh71ZRr2HKwYi9pd1s9pnuYkY').sheet1
+#print("Input bill number")
+#billnum = raw_input()
+
+id = '42zo6y'
+bill = "B226"
 
 
 
+def VoteCount(thread,billnum):
+    column = wks.find(billnum).col
+
+    already_done = []
+    submission = r.get_submission(submission_id='42zo6y')
+    comments = praw.helpers.flatten_tree(submission.comments)
+
+    for comment in comments:
+        if comment.id not in already_done:
+            print comment.body
+            print comment.author
+            if "Aye" in comment.body:
+                already_done.append(comment.id)
+                row = wks.find(comment.author).row
+                wks.update_cell(row,column,"Aye")
+
+            if "Nay" in comment.body:
+                already_done.append(comment.id)
+                row = wks.find(comment.author).row
+                wks.update_cell(row,column,"Nay")
+
+
+
+
+
+
+
+
+VoteCount(id,bill)
