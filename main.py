0427602d380536c5c4a28952c7a76cad570c04b4
chrispytoast123 <christospapandreou12@gmail.com>
2016-01-31 11:02:41 -0600

    Slightly Broken

--- main.py
+++ main.py
@@ -49,20 +49,23 @@ def VoteCount(thread,billnum):
         if comment.id not in already_done:
             print(comment.body)
             print(comment.author)
-			try:
-				already_done.append(comment.id)
-				if "content" in str(comment.body).lower() and "not" not in str(comment.body).lower():
-					row = wks.find(str(comment.author).lower()).row
-					val = wks.cell(row,column).value
-					if "N/A" not in val:
-						wks.update_cell(row,column,"Con")
-				if "content" in str(comment.body).lower() and "not" in str(comment.body).lower():
-					row = wks.find(str(comment.author).lower()).row
-					val = wks.cell(row,column).value
-					if "N/A" not in val:
-						wks.update_cell(row,column,"Not")
-			except gspread.exceptions.CellNotFound:
-                print("Automod Comment")
+			if "content" in str(comment.body).lower() and "not" not in str(comment.body).lower():
+				row = wks.find(str(comment.author).lower()).row
+				val = wks.cell(row,column).value
+				if "N/A" not in val:
+					wks.update_cell(row,column,"Con")
+					already_done.append(comment.id)
+					
+			if "content" in str(comment.body).lower() and "not" in str(comment.body).lower():
+				row = wks.find(str(comment.author).lower()).row
+				val = wks.cell(row,column).value
+				if "N/A" not in val:
+					wks.update_cell(row,column,"Not")
+					already_done.append(comment.id)
+					
+		except gspread.exceptions.CellNotFound:
+                already_done.append(comment.id)
+				print("Automod Comment")
                 
                 
                
