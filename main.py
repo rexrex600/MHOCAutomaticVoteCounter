d7cfe42e8e0e6eb39e9217d6557e87c52333a76b
chrispytoast123 <christospapandreou12@gmail.com>
2016-01-31 10:52:35 -0600

    Added Automod

--- main.py
+++ main.py
@@ -50,21 +50,19 @@ def VoteCount(thread,billnum):
             print(comment.body)
             print(comment.author)
 			try:
+				already_done.append(comment.id)
 				if "content" in str(comment.body).lower() and "not" not in str(comment.body).lower():
 					row = wks.find(str(comment.author).lower()).row
 					val = wks.cell(row,column).value
 					if "N/A" not in val:
 						wks.update_cell(row,column,"Con")
-						already_done.append(comment.id)
 				if "content" in str(comment.body).lower() and "not" in str(comment.body).lower():
 					row = wks.find(str(comment.author).lower()).row
 					val = wks.cell(row,column).value
 					if "N/A" not in val:
 						wks.update_cell(row,column,"Not")
-						already_done.append(comment.id)
 			except gspread.exceptions.CellNotFound:
                 print("Automod Comment")
-				already_done.append(comment.id)
                 
                 
                
